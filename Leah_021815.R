6b5d35ef361acca83a263a24bd31a5c49a0cad38
Leah <lreisman@princeton.edu>
2015-02-19 12:20:37 -0500

    Fixing and improving code

--- Leah_021815.R
+++ Leah_021815.R
@@ -1,26 +1,23 @@
 library(dplyr)
 library(xlsx)
-censo.1990 <- read.xlsx(file="popless2500_censo1900.xlsx", 1, header=FALSE)
+censo.1990 <- read.xlsx(file="popless2500_censo1900.xlsx", 1, header=FALSE, startRow=6, endRow=2610)
 
 #taking off weird rows on top and selecting the variables we care about
-censo.1990 <- slice(censo.1990, 5:1000)
 censo.1990 <- select(censo.1990, X1:X4)
+colnames(censo.1990) <- c("Clave", "Nombre", "Total", "Menos.2500")
 
 #removing missing values, takes out 37ish municipalities
 censo.1990[censo.1990 == ""] <- NA
-censo.1990 <- filter(censo.1990, !is.na(X3) | !is.na(X4))
-filter(censo.1990, is.na(X3) | is.na(X4)) #to see what was dropped
+censo.1990 <- filter(censo.1990, !is.na(Total) | !is.na(Menos.2500))
+filter(censo.1990, is.na(Total) | is.na(Menos.2500) #to see what was dropped
 
 #removing state overalls
-a <- c(1:18, "01", "02", "03", "04", "05", "06", "07", "08", "09")
-censo.muni <- filter(censo.1990, !(X1 %in% a))
-filter(censo.1990, X1 %in% a) #for reference. Where is Oaxaca? See the note at the end of this script
-
-#renaming and removing extra row, should have done this earlier
-colnames(censo.muni) <- c("Clave", "Nombre", "Total", "Menos.2500")
-censo.muni <- slice(censo.muni, 2:909)
+a <- c(1:32, "01", "02", "03", "04", "05", "06", "07", "08", "09")
+censo.muni <- filter(censo.1990, !(Clave %in% a))
+filter(censo.1990, Clave %in% a) #for reference. 
 
 #finding and filtering for percent of people living in towns of less than 2,500 for all municipalities
+#STOPPED HERE TO DEAL WITH OAXACA
 censo.muni$Total <- as.numeric(censo.muni$Total) #I don't know why this was coded incorrectly.
 censo.muni$Menos.2500 <- as.numeric(censo.muni$Menos.2500) #I don't know why this was coded incorrectly.
 censo.muni <- mutate(censo.muni, pct.rural = Menos.2500/Total)
